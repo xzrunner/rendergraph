90d3bd3fe1415a3ff14385a94e6ce41798989cb0
zz <xzrunner@gmail.com>
2020-04-23 08:57:08 +0800

    [CHANGED] to raw ptr, set from ref

--- include/rendergraph/RenderContext.h
+++ include/rendergraph/RenderContext.h
@@ -18,7 +18,7 @@ class RenderContext : public dag::Context
 public:
     ur2::RenderState ur_rs;
     std::shared_ptr<ur2::Context> ur_ctx = nullptr;
-    std::shared_ptr<ur2::Device>  ur_dev = nullptr;
+    const ur2::Device* ur_dev = nullptr;
 
     sm::mat4 cam_proj_mat;
     sm::mat4 cam_view_mat;

a693540e182428cd10f7678aeb733526baaedbf1
zz <xzrunner@gmail.com>
2020-10-05 07:43:07 +0800

    clean up

--- include/rendergraph/node/Shader.parm.h
+++ include/rendergraph/node/Shader.parm.h
@@ -3,12 +3,10 @@
 #endif
 
 #define NOT_GEN_SETTER_GETTER_FUNC
-#define PROP_EDIT_CODE
 
 PARAM_INFO(Vert, std::string, vert, m_vert, ())
 PARAM_INFO(Frag, std::string, frag, m_frag, ())
 
 #undef NOT_GEN_SETTER_GETTER_FUNC
-#undef PROP_EDIT_CODE
 
 PARAM_INFO(Language, rendergraph::node::Shader::Language, lang, m_lang, (rendergraph::node::Shader::Language::GLSL))
